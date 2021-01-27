0da9aca8fdcd72249369982143f0b47321caa0e6
Tommy Nienchi Chen <tommynchen@gmail.com>
2021-01-27 13:16:30 -0800

    working on assets superseeder

--- backend/routes/api/coinmarketcapAPI.js
+++ backend/routes/api/coinmarketcapAPI.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const fetch = require("node-fetch");
+// import cats from "./mockData/cats.json";
+const { dataStep1 } = require("./dataStep1.js");
+//import dataStep1 from "./dataStep1.json";
 
 const db = require("../../db/models");
 const router = express.Router();
@@ -20,7 +23,7 @@ const seederAssetsArray = async () => {
     }
   );
   const data = await response.json();
-  const dataStep1 = data.data.map((ele) => {
+  const dataStep1 = await data.data.map((ele) => {
     return {
       cmcId: ele.id,
       name: ele.name,
@@ -43,31 +46,13 @@ const seederAssetsArray = async () => {
     const description = { description: data.data[ele.cmcId] };
     return Object.assign(ele, description);
   });
+
   return dataStep2;
 };
 
 //test
 router.get(
   "/",
-  asyncHandler(async (req, res) => {
-    const response = await fetch(
-      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=50&convert=USD&logo&description&urls",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "X-CMC_PRO_API_KEY": coinmarketcapKey,
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    res.send(data);
-  })
-);
-
-//super seederfile test
-router.get(
-  "/seeder",
   asyncHandler(async (req, res) => {
     const response = await fetch(
       "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=50&convert=USD",
@@ -78,43 +63,81 @@ router.get(
         },
       }
     );
-
     const data = await response.json();
-    const dataStep1 = await Promise.all(
-      data.data.map((ele) => {
-        return {
-          cmcId: ele.id,
-          name: ele.name,
-          symbol: ele.symbol,
-          tags: ele.tags,
-          quote: ele.quote,
-        };
-      })
-    );
-    const dataStep2 = await Promise.all(
-      dataStep1.map(async (ele) => {
-        const response = await fetch(
-          `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=${ele.cmcId}`,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              "X-CMC_PRO_API_KEY": coinmarketcapKey,
-            },
-          }
-        );
-        const data = await response.json();
-        const assetDetails = await {
-          assetDetails: data.data[ele.cmcId],
-        };
+    const dataStep1 = await data.data.map((ele) => {
+      return {
+        cmcId: ele.id,
+        name: ele.name,
+        symbol: ele.symbol,
+        tags: ele.tags,
+        quote: ele.quote,
+      };
+    });
 
-        const ret = await Object.assign(ele, assetDetails);
+    res.json(dataStep1);
+  })
+);
 
-        return ret;
-      })
-    );
+//super seederfile test
+router.get(
+  "/seeder",
+  asyncHandler(async (req, res) => {
+    console.log(dataStep1);
+    const dataStep2 = [];
+    for (let i = 0; i < dataStep1.length; i++) {
+      const response = await fetch(
+        `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=${dataStep1[i].cmcId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-CMC_PRO_API_KEY": coinmarketcapKey,
+          },
+        }
+      );
+      const data = await response.json();
+      const assetDetails = await {
+        assetDetails: data.data[dataStep1[i].cmcId],
+      };
 
-    res.send(dataStep2);
+      const ret = await Object.assign(dataStep1[i], assetDetails);
+      dataStep2.push(ret);
+    }
+    // await dataStep1.forEach(async (ele) => {
+    //   const response = await fetch(
+    //     `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=${ele.cmcId}`,
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         "X-CMC_PRO_API_KEY": coinmarketcapKey,
+    //       },
+    //     }
+    //   );
+    //   const data = await response.json();
+    //   const assetDetails = await {
+    //     assetDetails: data.data[ele.cmcId],
+    //   };
+
+    //   const ret = await Object.assign(ele, assetDetails);
+    //   await dataStep2.push(ret);
+    // });
+
+    res.json(dataStep2);
   })
 );
 
 module.exports = router;
+
+// const dataStep2 = dataStep1.map(async (ele) => {
+//   const response = await fetch(
+//     `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=${ele.cmcId}`,
+//     {
+//       headers: {
+//         "Content-Type": "application/json",
+//         "X-CMC_PRO_API_KEY": coinmarketcapKey,
+//       },
+//     }
+//   );
+//   const data = await response.json();
+//   const description = { description: data.data[ele.cmcId] };
+//   return Object.assign(ele, description);
+// });
--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -6,103 +6,109 @@ import "./SignupForm.css";
 
 function SignupFormPage() {
   const dispatch = useDispatch();
+
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [disabledButton, setDisabledButton] = useState("true");
+
   const [validationErrors, setValidationErrors] = useState([]);
-  const [errors, setErrors] = useState([]);
 
-  // if (sessionUser) return <Redirect to="/" />;
-  // const validate = () => {
-  //   const validationErrors = [];
-  //   if (username.length > 30)
-  //     validationErrors.push("usernames must be less than 30 chars");
-  //   const emailPattern = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-  //   if (!emailPattern.test(email))
-  //     validationErrors.push("Enter a valid email address");
-  //   if (password !== confirmPassword)
-  //     validationErrors.push("Password doesnt not match");
-  //   return validationErrors;
-  // };
+  useEffect(() => {
+    setValidationErrors([]);
+    const errorsArr = validate();
+    setValidationErrors((prev) => [...prev, ...errorsArr]);
+    if (validationErrors.length === 0) {
+      setDisabledButton(false);
+    } else {
+      setDisabledButton(true);
+    }
+  }, [username, email, password]);
 
-  // useEffect(() => {
-  //   setValidationErrors([]);
-  //   const errorsArr = validate();
-  //   setValidationErrors((prev) => [...prev, ...errorsArr]);
-  // }, [username, email, password, confirmPassword]);
+  if (sessionUser) return <Redirect to="/" />;
+  const validate = () => {
+    const validationErrors = [];
+    // const emailPattern = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    // if (!emailPattern.test(email))
+    //   validationErrors.push("Enter a valid email address");
+    if (!/[A-Z]+/.test(password))
+      validationErrors.push("One uppercase character");
+    if (!/[a-z]+/.test(password))
+      validationErrors.push("One lowercase character");
+    if (!/[0-9]+/.test(password)) validationErrors.push("One number");
+    if (password.length < 8) validationErrors.push("8 characters minimum");
 
+    return validationErrors;
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-      setErrors([]);
-      return dispatch(
-        sessionActions.signup({ email, username, password })
-      ).catch((res) => {
-        if (res.data && res.data.errors) setErrors(res.data.errors);
-      });
-    }
-    return setErrors([
-      "Confirm Password field must be the same as the Password field",
-    ]);
+    setValidationErrors([]);
+    return dispatch(sessionActions.signup({ email, username, password })).catch(
+      (res) => {
+        if (res.data && res.data.errors) setValidationErrors(res.data.errors);
+      }
+    );
   };
 
   return (
-    <form className="signup-form" onSubmit={handleSubmit}>
-      <h2>
-        The fiat industrial era is about to rocket launch into the digital age
-      </h2>
-      <h6>
-        Sign up to follow my crypto portfolio and to explore my first solo full
-        stack web project postgre-express-redux-react learning project!
-      </h6>
-      <div>
-        <label for="signup-email">Email</label>
-        <input
-          id="signup-email"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label for="signup-username">Username</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label for="signup-password">Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Sign Up</button>
-      <ul>
-        {validationErrors.map((err) => (
-          <li key={err}>{err}</li>
-        ))}
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-    </form>
+    <div className="signup-page-container">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>
+          The fiat industrial era is about to rocket launch into the digital age
+        </h2>
+        <h5>
+          Sign up to follow my crypto portfolio and to explore my first solo
+          full stack web project postgre-express-redux-react learning project!
+        </h5>
+        <div>
+          <label for="signup-email">Email</label>
+          <input
+            id="signup-email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label for="signup-username">Username</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label for="signup-password">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <ul>
+          {validationErrors.map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+        <div className="signup-button-div">
+          <button
+            value={disabledButton}
+            disabled={disabledButton}
+            type="submit"
+          >
+            Sign Up
+          </button>
+          <p>
+            Create an account with the best none custodial crypto asset
+            portfolio tracker
+          </p>
+        </div>
+      </form>
+    </div>
   );
 }
 
